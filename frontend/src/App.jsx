689dae3c976389da18b5634033e148fb1a2331d5
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-12-08 18:56:16 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [jokes,setjokes] = useState([])
 
   useEffect(()=>{
-    axios.get('http://localhost:3000/jokes')
+    axios.get('/api/jokes')
     .then ((response)=>{
       setjokes(response.data)
     }
@@ -23,12 +23,12 @@ function App() {
      <h1>Rishabh Singh</h1>
      <p>JOKES: {jokes.length}</p>
      {
-      jokes.map((joke,index)=>{
+      jokes.map((joke,index)=>(
         <div key={joke.id}>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
-      })
+      ))
      }
     </>
   )
