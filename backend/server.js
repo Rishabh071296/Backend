f90007d49cacd0a96f980570847ba21dfb5fcd4c
Rishabh singh <125394319+Rishabh071296@users.noreply.github.com>
2023-12-08 14:57:17 +0530

    Update

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,40 @@ app.get ('/', (req,res)=>{
 
     res.send ('Server is ready');
 });
-const = port
+
+//get a list of 5 jokes
+
+app.get('/jokes', (req,res)=>{
+const jokes = [
+    {
+        id:1,
+        title:'A Jokes',
+        content : 'This is a first joke'
+    },
+    {
+        id:2,
+        title:'Another Jokes',
+        content: 'This is a another joke'
+    },
+    {
+        id:3,
+        title:'3rd Jokes',
+        content: 'This is a 3rd joke'
+    },
+    {
+        id:4,
+        title:'4th Jokes',
+        content: 'This is a 4th joke'
+    },
+    {
+        id:5,
+        title:'5th Jokes',
+        content: 'This is a 5th joke'
+    }
+];
+res.send(jokes);
+});
+
 const port = process.env.PORT || 3000;
 
 app.listen(port,()=>{
